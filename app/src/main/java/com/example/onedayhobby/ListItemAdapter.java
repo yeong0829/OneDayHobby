98831f5f69c84965c8b62e365e997a9d3ffb44f2
kdy1242 <dy12311229@gmail.com>
2021-12-10 09:28:39 +0900

    리스트 체크박스

--- app/src/main/java/com/example/onedayhobby/ListItemAdapter.java
+++ app/src/main/java/com/example/onedayhobby/ListItemAdapter.java
@@ -18,43 +18,57 @@ import java.util.List;
 
 public class ListItemAdapter extends BaseAdapter{
 
-    Context mContext = null;
-    LayoutInflater mLayoutInflater = null;
-    ArrayList<ListItem> sample;
+    // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
+    private ArrayList<ListItem> listViewItemList = new ArrayList<ListItem>() ;
 
-    public ListItemAdapter(Context context, ArrayList<ListItem> data) {
-        mContext = context;
-        sample = data;
-        mLayoutInflater = LayoutInflater.from(mContext);
+    // ListViewAdapter의 생성자
+    public ListItemAdapter() {
+
     }
 
+    // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
     @Override
     public int getCount() {
-        return sample.size();
+        return listViewItemList.size() ;
     }
 
+    // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
+
+        // "listview_item" Layout을 inflate하여 convertView 참조 획득.
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+        TextView listTextView = (TextView) convertView.findViewById(R.id.list_content) ;
+
+        // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
+        ListItem listViewItem = listViewItemList.get(position);
+
+        // 아이템 내 각 위젯에 데이터 반영
+        listTextView.setText(listViewItem.getText());
+
+        return convertView;
+    }
+
+    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
     @Override
     public long getItemId(int position) {
-        return position;
+        return position ;
     }
 
+    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
     @Override
-    public ListItem getItem(int position) {
-        return sample.get(position);
+    public Object getItem(int position) {
+        return listViewItemList.get(position) ;
     }
 
-    public ListItem setItem(int position) {
-        return sample.get(position);
-    }
-
-    @Override
-    public View getView(int position, View converView, ViewGroup parent) {
-        View view = mLayoutInflater.inflate(R.layout.list_item, null);
-
-        TextView content = (TextView)view.findViewById(R.id.content);
-
-        content.setText(sample.get(position).getText());
-
-        return view;
+    // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
+    public void addItem(String text) {
     }
 }
--- app/src/main/java/com/example/onedayhobby/MainActivity.java
+++ app/src/main/java/com/example/onedayhobby/MainActivity.java
@@ -26,11 +26,6 @@ public class MainActivity extends AppCompatActivity {
     private ListView hobbyListView;
     private ListView doListView;
 
-    private boolean isFabOpen = false; // Fab 버튼 default 는 닫혀있음
-
-    private FloatingActionButton fabMain;
-    private FloatingActionButton fabDel;
-    private FloatingActionButton fabEdit;
     private ImageButton backBtn;
 
     @Override
@@ -41,41 +36,8 @@ public class MainActivity extends AppCompatActivity {
         /* 위젯과 멤버변수 참조 획득 */
         hobbyListView = (ListView)findViewById(R.id.hobby_list);
 
-        /* 위젯과 멤버변수 참조 획득 */
-        doListView = (ListView)findViewById(R.id.do_list);
-
         /* 아이템 추가 및 어댑터 등록 */
         dataSetting();
-
-        // 메인 플로팅 버튼 클릭
-        fabMain = findViewById(R.id.info_fab);
-        fabMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleFab();
-            }
-        });
-
-        // 삭제 플로팅 버튼 클릭
-        fabDel = findViewById(R.id.info_fab_del);
-        fabDel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "삭제 버튼 클릭", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // 추가 플로팅 버튼 클릭
-        fabEdit = findViewById(R.id.info_fab_edit);
-        // final TextView plan_test = (TextView)findViewById(R.id.plan_test);
-        fabEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CustomDialog customDialog = new CustomDialog(MainActivity.this);
-
-                // customDialog.callFunction(plan_test);
-            }
-        });
 
         // 뒤로가기
         backBtn = findViewById(R.id.tos_pass_btn);
@@ -112,38 +74,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        ArrayList<ListItem> DoList = new ArrayList<>();
-        for (int i=0; i<10; i++) {
-            DoList.add(new ListItem("dd"));
-        }
-
-        // 할일 리스트
-//        ListItemAdapter listItemAdapter = new ListItemAdapter(this, DoList);
-//        doListView.setAdapter(listItemAdapter);
-
-    }
-
-    //fab 나오게하는 애니메이션
-    private void toggleFab() {
-        Log.d("TAG", "toggleFab: fab 애니메이션 실행");
-        // 플로팅 액션 버튼 닫기 - 열려있는 플로팅 버튼 집어넣는 애니메이션
-        if (isFabOpen) {
-            ObjectAnimator fd_animation = ObjectAnimator.ofFloat(fabDel, "translationY", 0f);
-            fd_animation.start();
-            ObjectAnimator fe_animation = ObjectAnimator.ofFloat(fabEdit, "translationY", 0f);
-            fe_animation.start();
-            ObjectAnimator fm_animation = ObjectAnimator.ofFloat(fabMain, View.ROTATION, -85f, 0f);
-            fm_animation.start();
-        } else {
-            // 플로팅 액션 버튼 열기 - 닫혀있는 플로팅 버튼 꺼내는 애니메이션
-            ObjectAnimator fd_animation = ObjectAnimator.ofFloat(fabDel, "translationY", -360f);
-            fd_animation.start();
-            ObjectAnimator fe_animation = ObjectAnimator.ofFloat(fabEdit, "translationY", -180f);
-            fe_animation.start();
-            ObjectAnimator fm_animation = ObjectAnimator.ofFloat(fabMain, View.ROTATION, 0f, -85f);
-            fm_animation.start();
-        }
-        isFabOpen = !isFabOpen;
     }
 }
